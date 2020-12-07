b33fa7db01f7707620dc04d4212a20b8bce47d06
Aravinda HB <aru03.info@gmail.com>
2020-12-07 16:56:04 +0530

    interfaces demo

--- Eclipse_WS/JavaTraining/src/com/synechron/java/basic/looping/ForEachDemo.java
+++ Eclipse_WS/JavaTraining/src/com/synechron/java/basic/looping/ForEachDemo.java
@@ -6,11 +6,11 @@ public class ForEachDemo {
 
 	public static void main(String[] args) {
 		String[] names = { "Aravinda ", "Ramesh", "Girish", "Mahesh", "Suresh" };
-		
-		
+
 		for (String name : names) {
 			System.out.println(name);
 		}
-
+		LoopsDemo.printTableUsingFor(10);
+		LoopsDemo.printMultipleTables(1, 20);
 	}
 }
